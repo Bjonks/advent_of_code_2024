246ffcfa27979dc8fb83b5b42392c3a8f7e04b3e
Bjonks <oandersson50@gmail.com>
2024-12-02 22:25:58 +0100

    Solution for day1 part two

--- Day_1/src/main.rs
+++ Day_1/src/main.rs
@@ -1,6 +1,12 @@
 use std::fs::read_to_string;
 
 fn main() {
+part_one();
+part_two();
+}
+
+
+fn part_one () {
     let mut first_number: Vec<i32> = Vec::new();
     let mut second_number: Vec<i32> = Vec::new();
 
@@ -13,15 +19,15 @@ fn main() {
         .filter_map(|n| n.parse::<i32>().ok())
         .collect();
 
-    if numbers.len() == 2 {
+        if numbers.len() == 2 {
 
         first_number.push(numbers[0]);
         second_number.push(numbers[1]);
 
 
-    } else {
+        } else {
         eprintln!("Invalid line format: {}", line);
-    }
+        }
 
     }
     first_number.sort();
@@ -31,9 +37,48 @@ fn main() {
 
     for i in 0..first_number.len() {
         answer += (first_number[i] - second_number[i]).abs();
-        println!("{} - {}", first_number[i], second_number[i]);
+        // println!("{} - {}", first_number[i], second_number[i]);
     }
 
+
+    println!("{}", answer);
+}
+
+fn part_two() {
+    let mut locations: (Vec<u32>, Vec<u32>) = (vec![], vec![]);
+
+    let mut answer = 0;
+    let input: String = read_to_string("src/input.txt")
+    .expect("Can't find file");
+
+    for line in input.lines() {
+        let numbers: Vec<u32> = line
+        .split_whitespace()
+        .filter_map(|n| n.parse::<u32>().ok())
+        .collect();
+
+        if numbers.len() == 2 {
+
+        locations.0.push(numbers[0]);
+        locations.1.push(numbers[1]);
+
+
+        } else {
+        eprintln!("Invalid line format: {}", line);
+        }
+
+    }
+
+    for i in 0..locations.0.len() {
+        let mut multiplier = 0;
+        for j in 0..locations.1.len() {
+            
+            if locations.0[i] == locations.1[j] {
+                multiplier += 1 
+            }
+        }
+        answer += locations.0[i] * multiplier;
+    }
 
     println!("{}", answer);
 }
