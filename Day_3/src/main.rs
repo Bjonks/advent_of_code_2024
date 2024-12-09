2eae07a5c9c8be1e2005e56751753ea922e439f6
Bjonks <oandersson50@gmail.com>
2024-12-09 23:07:12 +0100

    Day3 complete

--- Day_3/src/main.rs
+++ Day_3/src/main.rs
@@ -6,9 +6,13 @@ fn main() {
     
     let mut answer_part_one: u32 = 0;
 
+    let mut answer_part_two: u32 = 0;
+
+    let mut valid: bool = true;
+
     let input = read_to_string("./src/input.txt").unwrap();
     let re_part_one = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let re_part_two = Regex::new(r"(do\(\)|don't\(\)|mul\((\d+),(\d+)\))").unwrap();
+    let re_part_two = Regex::new(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)").unwrap();
 
     for cap in re_part_one.captures_iter(&input) {
 
@@ -18,7 +22,21 @@ fn main() {
         answer_part_one += num1 * num2;
     }
 
+    for cap in re_part_two.captures_iter(&input) {
+
+        if &cap[0] == "do()" {
+            valid = true;
+        } else if &cap[0] == "don't()" {
+            valid = false;
+        } else if valid {
+            let num1: u32 = cap[2].parse().unwrap();
+            let num2: u32 = cap[3].parse().unwrap();
+    
+            answer_part_two += num1 * num2;
+        }
+    }
 
 
     println!("{}", answer_part_one);
+    println!("{}", answer_part_two);
 }
